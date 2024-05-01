e8911a53da6058c35ea2e512e653e19c154f7e9e
sandysalamaa <138966757+sandysalamaa@users.noreply.github.com>
2024-05-01 14:27:13 +0300

    Update asgi.py

--- my_learning_club/asgi.py
+++ my_learning_club/asgi.py
@@ -7,22 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-# import os
-# from django.core.asgi import get_asgi_application
-# from channels.routing import ProtocolTypeRouter,URLRouter
+
 from channels.security.websocket import AllowedHostsOriginValidator  # only hosts who have access to this websocket
 from channels.auth import AuthMiddlewareStack
-# from my_learning_club import routing
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_learning_club.settings')
-
-# django_asgi_application = get_asgi_application()
-
-
-# application = ProtocolTypeRouter({ 
-#                                   'http':django_asgi_application,
-#                                   'websocket':AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)))
-#                                   })
 
 
 import os
